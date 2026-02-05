24b68e1130ccf82218a0c1a2fe7c0e641afb401f
Alex Benedict Selva <alexalex1772006@gmail.com>
2026-02-05 13:03:24 +0530

    made home,login page responsive

--- my-app/src/pages/Home/components/Header1.jsx
+++ my-app/src/pages/Home/components/Header1.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css';
 import LOGO from '../../image/logo.dr.png'
 import { Link,useNavigate } from 'react-router-dom';
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
+  
   const GotoHome = () => {
     navigate('/');
   }
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  }
+  
   return (
     <div>
       <section id="header">
         <img onClick={GotoHome} src={LOGO} alt='' />
-        <div >
+        
+        {/* Desktop Navigation */}
+        <div className="desktop-nav">
             <ul id="navbar">
                 <Link to='/' className='link'><li>Home</li></Link>
                 <Link to='/cart' className='link'><li>Cart</li></Link>
@@ -20,6 +33,23 @@ const Header = () => {
                 <Link to='/' className='link'><li>About</li></Link>
                 <Link to='/' className='link'><li>Contact</li></Link>
             </ul>
+        </div>
+        
+        {/* Mobile Menu Button */}
+        <div className="mobile-menu-btn" onClick={toggleMenu}>
+          <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+        </div>
+        
+        {/* Mobile Dropdown Menu */}
+        <div className={`mobile-dropdown ${isMenuOpen ? 'active' : ''}`}>
+          <ul>
+            <Link to='/' className='link' onClick={closeMenu}><li>Home</li></Link>
+            <Link to='/cart' className='link' onClick={closeMenu}><li>Cart</li></Link>
+            <Link to='/shop' className='link' onClick={closeMenu}><li>Shop</li></Link>
+            <Link to='/' className='link' onClick={closeMenu}><li>Blog</li></Link>
+            <Link to='/' className='link' onClick={closeMenu}><li>About</li></Link>
+            <Link to='/' className='link' onClick={closeMenu}><li>Contact</li></Link>
+          </ul>
         </div>
     </section>
     </div>
